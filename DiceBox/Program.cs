c2eee4c0dc77210172a8419fb8c014696c95f31f
Joeking135 <ArcticBreeze137@gmail.com>
2023-03-13 18:29:53 +0000

    Fixed some issues and sort of cleaned up (sort of)
    It still makes me feel ill

--- DiceBox/Program.cs
+++ DiceBox/Program.cs
@@ -10,29 +10,40 @@ namespace DiceBox
         static void Main(string[] args)
         {
             int playCount = 0; //This is used to determine each players go.
+            int player;
 
             DiceBox player1 = new();
             DiceBox player2 = new(); //initiate objects of each player
 
             while (true)
             {
+                player = (playCount % 2) + 1;
 
-                if (playCount % 2 == 0) //Determines whose go it is (alternates after each cycle).
+                Console.WriteLine($"Player {player} - press enter to roll the dice."); Console.ReadLine();
+                Console.Write($"Player {player} rolled a ");
+
+                if (player == 1) //Determines whose go it is (alternates after each cycle).
                 {
-                    Console.WriteLine("Player 1 - press enter to roll the dice."); Console.ReadLine();
-                    Console.WriteLine( $"Player 1 rolled a {player1.RollDice()}"); //Rolls the dice, returning an integer value (to display to the user), but this method also updates player 1's boolean array.
-                    Console.WriteLine("\nPLAYER 1 SCOREBOARD");
+                    Console.WriteLine(player1.RollDice()); //Rolls the dice, returning an integer value (to display to the user), but this method also updates player 1's boolean array.
+                    Console.WriteLine($"PLAYER {player} SCORESHEET \n");
                     player1.DisplayBox(); //Displays player one's boolean array values.
-
-                    
                 }
-                else //Same as player one but with player two (I couldn't find a way of optimizing this part).
+                else //Same as player one but with player two 
                 {
-                    Console.WriteLine("Player 2 - press enter to roll the dice."); Console.ReadLine();
-                    Console.WriteLine($"Player 2 rolled a {player2.RollDice()}");
-                    Console.WriteLine("\nPLAYER 2 SCOREBOARD");
+                    Console.WriteLine(player2.RollDice());
+                    Console.WriteLine($"PLAYER {player} SCORESHEET \n"); //Unsure on how to prevent this repeated code
                     player2.DisplayBox();
                 }
+
+                if (player1.Duplicate || player2.Duplicate)
+                {
+                    Console.WriteLine($"\n PLAYER {player} ROLLED A DUPLICATE"); 
+                    player1.Duplicate = false; player2.Duplicate = false; //Yuck... 
+                    
+                }
+
+                Console.WriteLine($"\nLives: Player 1 = {player1.Lives}\n       Player 2 = {player2.Lives}\n"); //outputs the lives of each player.
+
 
                 if (player1.CheckWin() || player2.Lives <= 0) //Checks whether each player has rolled all of the possible integers, or if the other player has run out of lives.
                 {
@@ -47,8 +58,6 @@ namespace DiceBox
 
                 Console.WriteLine("Hit a key"); Console.ReadKey(); Console.Clear();
                 playCount++; 
-                
-
 
             }
 
@@ -57,18 +66,21 @@ namespace DiceBox
 
     public class DiceBox 
     {
-        public bool[] diceValues { get; set; } //Boolean array that stores which values have been rolled.
+        public bool[] DiceValues { get; set; } //Boolean array that stores which values have been rolled.
 
         public int Lives { get; set; } //Stores the unique player's lives.
+
+        public bool Duplicate { get; set; } //Boolean to represent if the player has rolled a duplicate
 
 
         public DiceBox() //Constructor.
         {
-            diceValues = new bool[13]; //Initiates the boolean array.
-            diceValues[0] = true; 
-            diceValues[1] = true; //Positions 0 and 1 are set to true as they cannot be rolled naturally.
+            DiceValues = new bool[13]; //Initiates the boolean array.
+            DiceValues[0] = true; 
+            DiceValues[1] = true; //Positions 0 and 1 are set to true as they cannot be rolled naturally.
 
             Lives = 3; //Sets the players lives to 3.
+            Duplicate = false;
 
         }
 
@@ -76,14 +88,20 @@ namespace DiceBox
         {
             Random rng = new();
 
+
             int total = rng.Next(1, 7) + rng.Next(1, 7);
 
-            if (diceValues[total] == true) //If this value has already been rolled, it deducts one from the players lives.
+            if (DiceValues[total] == true) //If this value has already been rolled, it deducts one from the players lives.
             {
                 Lives--;
-                Console.WriteLine($"That is a duplicate, lives = {Lives}"); //This outputs in the wrong order but ... oh well.
+                Duplicate = true; //Used to output text later.
             }
-            diceValues[total] = true; //Sets the value at the location of total to true.
+            else
+            {
+                Duplicate = false;
+                DiceValues[total] = true; //Sets the value at the location of total to true.
+            }
+            
             return total; //returns the outcome of the dice roll (to display to the user).
 
         }
@@ -91,15 +109,25 @@ namespace DiceBox
         public void DisplayBox() //This is a simple for loop which outputs the values in the array from 2-12.
         {
 
-            for (int i = 2; i < diceValues.Length; i++)
+            for (int i = 2; i < DiceValues.Length; i++)
             {
-                Console.WriteLine($"{i} = {diceValues[i]}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                if (DiceValues[i] == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.WriteLine($"{i} = {DiceValues[i]}");
+
+                
             }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
         }
 
         public bool CheckWin() //This checks whether the entire array is set to true (and if so, the player has won).
         {
-            if (Array.TrueForAll(diceValues, e => e.Equals(true)))
+            if (Array.TrueForAll(DiceValues, e => e.Equals(true)))
             {
                 return true;
             }
