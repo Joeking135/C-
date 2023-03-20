9179edcb896df434a7cac1a3d2e419c4d2568d1f
Joeking135 <ArcticBreeze137@gmail.com>
2023-03-20 13:02:14 +0000

    Added perfect numbers project
    Still awaiting some optimisation

---
+++ Perfect Numbers/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Perfect_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> perfectNumbers = new List<long>();
+            int number = 1;
+
+
+            Console.WriteLine("The first 5 perfect numbers are...");
+            while (perfectNumbers.Count < 5)
+            {
+                if (IsPerfect(number))
+                {
+                    perfectNumbers.Add(number);
+                }
+                number++;
+            }
+
+
+            
+            foreach (long item in perfectNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        static bool IsPerfect(long number)
+        {
+            long sum = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum == number;
+        }
+
+
+
+    }
+
+    
+}
