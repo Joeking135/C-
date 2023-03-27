9405c648f599f187c136a6852d4adc4f022bc264
Joeking135 <ArcticBreeze137@gmail.com>
2023-03-27 22:25:35 +0100

    Added TCP stuff
    This will soon get its own repos, ready for the main project.

---
+++ TCPclient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TCPclient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("localhost", 8888);
+                Console.WriteLine("Connected to server.");
+
+                NetworkStream stream = client.GetStream();
+
+                // Send a message to the server.
+                string message = "Hello, server!";
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", message);
+
+                // Read the server's response.
+                data = new byte[1024];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                message = Encoding.ASCII.GetString(data, 0, bytesRead);
+                Console.WriteLine("Received: {0}", message);
+
+                // Close the connection.
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCPserver/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPserver
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+            listener.Start();
+            Console.WriteLine("Server started.");
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    Console.WriteLine("Client connected.");
+
+                    // Handle the client in a separate task.
+                    Task.Run(() => HandleClientAsync(client));
+                }
+            }
+            finally
+            {
+                listener.Stop();
+                Console.WriteLine("Server stopped.");
+            }
+        }
+
+        static async Task HandleClientAsync(TcpClient client)
+        {
+            try
+            {
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine("Received: {0}", message);
+
+                        // Echo the message back to the client.
+                        byte[] response = Encoding.ASCII.GetBytes(message);
+                        await stream.WriteAsync(response, 0, response.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+            finally
+            {
+                client.Close();
+                Console.WriteLine("Client disconnected.");
+            }
+        }
+    }
+}
