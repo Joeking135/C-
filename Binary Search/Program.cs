d22d119839bccb42b548f0a823485e0218f5b576
Joeking135 <ArcticBreeze137@gmail.com>
2023-03-31 14:08:01 +0100

    Revised Repos
    Added Binary search and removed all old files, which will now be stored on my laptop. If you require any of these, dm me.

---
+++ Binary Search/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {2, 6, 12, 32, 64, 98, 102};
+
+            Console.Write("Input Number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (BinarySearh(array, input, 0, array.Length))
+            {
+                Console.WriteLine("Yes") ;
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+        static bool BinarySearh(int[] array,int input, int low, int high){
+
+                if (low > high)
+                {
+                    return false;
+                }
+
+                int midPoint = (low + high) / 2;
+                if ( array[midPoint] == input)
+                {
+                    return true; 
+                }
+                else if (array[midPoint] < input)
+                {
+                    return BinarySearh(array, input, midPoint + 1, high);
+                } 
+                else if(array[midPoint] > input)
+                {
+                    return BinarySearh(array, input, low, midPoint - 1);
+                }
+
+            return true;
+        }
+    }
+}
