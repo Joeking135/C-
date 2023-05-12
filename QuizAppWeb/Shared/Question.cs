44ae0a5c29c9515bc370ae973062348d5c6cd3e5
Joeking135 <ArcticBreeze137@gmail.com>
2023-05-12 18:57:38 +0100

    Added Quiz web App
    Still very much a work in progress at this stage - I will be working on this over the week.

---
+++ QuizAppWeb/Shared/Question.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizAppWeb.Shared
+{
+	public class Question
+	{
+		private string _Q;
+		/// <summary>
+		/// The Question to be asked
+		/// </summary>
+		public string QuestionText
+		{
+			get { return _Q; }
+			set { _Q = value; }
+		}
+		private string _A1;
+		/// <summary>
+		/// The first possible answer
+		/// </summary>
+		public string Answer1
+		{
+			get { return _A1; }
+			set { _A1 = value; }
+		}
+		private string _A2;
+		/// <summary>
+		/// The second possible answer
+		/// </summary>
+		public string Answer2
+		{
+			get { return _A2; }
+			set { _A2 = value; }
+		}
+		private string _A3;
+		/// <summary>
+		/// The third possible answer
+		/// </summary>
+		public string Answer3
+		{
+			get { return _A3; }
+			set { _A3 = value; }
+		}
+		private string _A4;
+		/// <summary>
+		/// The fourth possible answer
+		/// </summary>
+		public string Answer4
+		{
+			get { return _A4; }
+			set { _A4 = value; }
+		}
+		private int _correct;
+		/// <summary>
+		/// The correct answer as a number 1-4.
+		/// </summary>
+		public int Correct
+		{
+			get { return _correct; }
+			set { _correct = value; }
+		}
+
+
+
+		public Question(string stringin) //constructor
+		{
+			string[] DataIn = stringin.Split(',');
+			_Q = DataIn[0];
+			_A1 = DataIn[1];
+			_A2 = DataIn[2];
+			_A3 = DataIn[3];
+			_A4 = DataIn[4];
+			_correct = int.Parse(DataIn[5]);
+		}
+	}
+}
---
+++ QuizAppWeb/Shared/Quiz.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace QuizAppWeb.Shared
+{
+	public class Quiz
+	{
+		private List<Question> _QuizQuestions = new List<Question>();
+		private int _QCount = 0;
+
+		/// <summary>
+		/// This is the question number up to for the quiz
+		/// </summary>
+		public int QuestionNumber
+		{
+			get { return _QCount; }
+			set { _QCount = value; }
+		}
+		private int _Score = 0;
+
+		/// <summary>
+		/// This is the score for his quiz
+		/// </summary>
+		public int Score
+		{
+			get { return _Score; }
+			set { _Score = value; }
+		}
+
+		/// <summary>
+		/// This tells you how many questions are in the quiz
+		/// </summary>
+		public int QuestionTotal
+		{
+			get { return _QuizQuestions.Count; }
+		}
+
+		/// <summary>
+		/// This tells you how many questions are left in the quiz
+		/// </summary>
+		public int QuestionsLeft
+		{
+			get { return _QuizQuestions.Count - (_QCount + 1); }
+		}
+
+		/// <summary>
+		/// This tells loads the questions.txt file which must be in the bin folder of the project.
+		/// </summary>
+		public Quiz()
+		{ 
+
+			Question q = new Question("﻿What is the capital of France?,London,Paris,Stockport,Marseille,2");
+			_QuizQuestions.Add(q);
+		}
+
+		/// <summary>
+		/// This method will indicate whether the answer number provided is true (correct) or false
+		/// if true it will also increment the quiz score by 1.
+		/// </summary>
+		/// <param name="answer">The answer number between 1 and 4 that you want to check</param>
+		/// <returns></returns>
+		public bool CheckAnswer(int answer)
+		{
+			if (answer == _QuizQuestions[_QCount - 1].Correct)
+			{
+				Score++;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+
+		/// <summary>
+		/// This gets the next question in the quiz. If there are no more questions it will return null
+		/// </summary>
+		/// <returns>a question object</returns>
+		public Question GetNextQuestion()
+		{
+			if (_QCount < _QuizQuestions.Count)
+			{
+				_QCount++;
+				return _QuizQuestions[_QCount - 1];
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// This gets the question in the quiz for the question number specified. If there is no question it will return null
+		/// </summary>
+		/// <param name="answer">The answer number between 1 and 4 that you want to check</param>
+		/// <returns>a question object</returns>
+		public Question GetQuestion(int q)
+		{
+			if (q < _QuizQuestions.Count)
+			{
+
+				return _QuizQuestions[q];
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+	}
+}
+
